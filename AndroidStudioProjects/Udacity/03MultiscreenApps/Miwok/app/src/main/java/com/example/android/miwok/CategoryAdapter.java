738ee5a0e3e0560140c8e96b426e71a3d854a094
Marc Grall <mc.grall@hotmail.de>
2017-09-28 23:48:04 +0200

    Hook up a TabLayout with the ViewPager.

--- AndroidStudioProjects/Udacity/03MultiscreenApps/Miwok/app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ AndroidStudioProjects/Udacity/03MultiscreenApps/Miwok/app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,10 +12,26 @@ import android.util.Log;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter (FragmentManager fm){
+    /** Context of the app */
+    private Context mContext;
+
+    /**
+     * Create a new CategoryAdapter object
+     * @param context needed to convert the string ID's into a actual char sequence, by calling
+     *                context.getString("").
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter (Context context, FragmentManager fm){
         super(fm);
+        mContext = context;
     }
 
+    /**
+     *
+     * @param position page number.
+     * @return the fragment that should be displayed for the given page number.
+     */
     @Override
     public Fragment getItem(int position) {
         switch (position) {
@@ -27,13 +44,33 @@ public class CategoryAdapter extends FragmentPagerAdapter {
             case 3:
                 return new PhrasesFragment();
             default:
-                Log.e("CategoryAdapter", "this should never happen");
+                Log.e("CategoryAdapter", "getItem: this should never happen");
                 return new NumbersFragment();
         }
     }
 
+    /**
+     * Return the total number of pages.
+     */
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.category_numbers);
+            case 1:
+                return mContext.getString(R.string.category_family);
+            case 2:
+                return mContext.getString(R.string.category_colors);
+            case 3:
+                return mContext.getString(R.string.category_phrases);
+            default:
+                Log.e("CategoryAdapter", "getPageTitle: this should never happen");
+                return mContext.getString(R.string.category_numbers);
+        }
     }
 }
